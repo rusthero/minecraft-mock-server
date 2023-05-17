ec93ab252022f5ca39b3bd0220340e2689861fbf
Kara <rusthero@proton.me>
2023-05-17 06:57:13 +0300

    Read handshake request

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+// TODO Create structs for types
+pub mod data {
+    use std::string::FromUtf8Error;
+
+    pub fn read_var_int(buf: &mut Vec<u8>) -> i32 {
+        let mut result: i32 = 0;
+        let mut shift: u32 = 0;
+        let mut index: usize = 0;
+
+        while let Some(byte) = buf.get(index).copied() {
+            result |= ((byte & 0x7F) as i32) << shift;
+            shift += 7;
+            index += 1;
+
+            if byte & 0x80 == 0 {
+                break;
+            }
+
+            if shift >= 32 {
+                panic!("invalid varint format");
+            }
+        }
+
+        buf.drain(..index);
+        result
+    }
+
+    pub fn read_string(buf: &mut Vec<u8>) -> Result<String, FromUtf8Error> {
+        let size: usize = read_var_int(buf) as usize;
+        let result = String::from_utf8(buf[..size].to_owned());
+
+        buf.drain(..size);
+        result
+    }
+
+    pub fn read_u16(buf: &mut Vec<u8>) -> u16 {
+        let result = u16::from_be_bytes(buf[..2].try_into().unwrap());
+
+        buf.drain(..2);
+        result
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::net::SocketAddr;
 
 use tokio::net::{TcpListener, TcpStream};
 
+use minecraft_server::data::{read_string, read_u16, read_var_int};
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:25565").await?;
-    println!("listening for clients");
+
+    println!("listening for clients (protocol v1.19.4)");
 
     loop {
         let (stream, addr) = listener.accept().await?;
@@ -16,13 +19,30 @@ async fn main() -> Result<()> {
 
 async fn handle_client(stream: TcpStream, addr: SocketAddr) -> Result<()> {
     println!("new client: {addr:?}");
+
     loop {
         stream.readable().await?;
-        let mut buf = [0; 64];
+
+        let mut buf = [0; 4096];
         match stream.try_read(&mut buf) {
             Ok(0) => break,
             Ok(n) => {
-                println!("read {n} bytes");
+                let mut buf = buf.to_vec();
+
+                let length = read_var_int(&mut buf);
+                let id = read_var_int(&mut buf);
+
+                match id {
+                    0 => {
+                        println!("received handshake packet");
+                        process_handshake(buf).await;
+                    }
+                    _ => {
+                        println!("received {n} size unknown packet");
+                    }
+                }
+
+                println!("packet length: {length}, id: {id:x?}")
             }
             Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
                 continue;
@@ -31,8 +51,16 @@ async fn handle_client(stream: TcpStream, addr: SocketAddr) -> Result<()> {
                 return Err(e);
             }
         }
-        println!("{buf:x?}");
     }
 
     Ok(())
+}
+
+async fn process_handshake(mut buf: Vec<u8>) {
+    let pro_ver = read_var_int(&mut buf);
+    let srv_addr = read_string(&mut buf).unwrap();
+    let srv_port = read_u16(&mut buf);
+    let next_state = read_var_int(&mut buf);
+
+    println!("protocol version: {pro_ver}, server address: {srv_addr}, server port: {srv_port}, next state: {next_state}");
 }
